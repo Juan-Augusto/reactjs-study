11fc07dc346964021632025741e26f564c34c470
juan-augusto <juanaugusto1@live.com>
2022-01-03 21:56:16 -0300

    Adição de Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Input from "./components/InputFunction/Input";
 import ListRender from "./components/Lista/List";
 import ExhibitionCondition from "./components/Condicional/Condition";
 import StyledCondition from "./components/EstilizacaoCondicional/StyleWithCondition";
+import { Container, Botao } from "./components/StyledComponents/StyledComponent";
 const App = () => {
   return (
     <>
@@ -11,6 +12,13 @@ const App = () => {
       <ListRender/>
       <ExhibitionCondition/>
       <StyledCondition/>
+      <Container>
+        <h1>Utilização de Styled Components</h1>
+        <Botao>
+          Texto do botao
+        </Botao>
+      </Container>
+
     </>
     
 
---
+++ src/components/StyledComponents/StyledComponent.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: blue;
+    color: white;
+
+`;
+export const Botao = styled.button`
+    font-size: 40px;
+`;
