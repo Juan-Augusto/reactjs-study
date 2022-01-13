db42fe868faf3f95c1911d3a2d57aa7f960ba975
juan-augusto <juanaugusto1@live.com>
2022-01-12 23:40:16 -0300

    Adiçãod de funções assíncronas

--- src/components/Request/MoviesRequest.jsx
+++ src/components/Request/MoviesRequest.jsx
@@ -2,21 +2,40 @@ import { useEffect, useState } from "react";
 
 const MovieRequest = () => {
     const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(false);
     useEffect(() =>{
         loadMovies();
     }, []);
-    const loadMovies = () => {
-        fetch('https://api.b7web.com.br/cinema/').then((response) => {
-            return response.json();
-        })
-        .then((json) => {
-            setMovies(json);
-        })
+    // const loadMovies = () => {
+    //     fetch('https://api.b7web.com.br/cinema/').then((response) => {
+    //         return response.json();
+    //     })
+    //     .then((json) => {
+    //         setMovies(json);
+    //     })
+    // }
+    const loadMovies = async () => {
+        setLoading(true);
+        let response = await fetch('https://api.b7web.com.br/cinema/');
+        let json = await response.json();
+        setLoading(false);
+        setMovies(json);
     }
+    
     return(
         <div>
+
+            {loading &&
+                <div>
+                    Carregando...
+                </div>
+            }
             <div>
-                total de Filmes: {movies.length} 
+                {!loading &&
+                    <div> 
+                        total de Filmes: {movies.length} 
+                    </div>
+                }
             </div>
             <div className="grid grid-cols-6 gap-3">
                 {movies.map((item, index) =>(
